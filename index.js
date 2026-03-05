c8de491fdda6f1f062b2ae298eaf2c6e3e0026ce
alihossen4 <mdahossen.dev@gmail.com>
2026-03-06 04:46:20 +0600

    update: serverles function applied

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // import bodyParser from "body-parser"
 const cors = require('cors')
 
-// import serverless from "serverless-http";
+import serverless from "serverless-http";
 const router = require("./src/routes/jobs.route.js")
 const {JobData} = require("./src/data/index.js")
 const express = require("express")
@@ -27,8 +27,8 @@ app.use('/filter',router,(req, res)=>{
   const data = JobData.filter((job)=>{job})
   res.json(data);
 })
-// export default serverless(app);
+export default serverless(app);
 
-app.listen(3000,()=>{
-  console.log("server is running at 3000");
-})
+// app.listen(3000,()=>{
+//   console.log("server is running at 3000");
+// })
